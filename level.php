3a7106b28bac4c6b9d9d5ea84dfa359eabf5b3c4
Hilmi Khaidar N <101361677+HilmiKhaidar@users.noreply.github.com>
2024-12-22 23:30:31 +0700

    Add files via upload

---
+++ level.php
@@ -0,0 +1,32 @@
+<?php 
+
+$level = 0;
+$levelMaks = 15;
+
+
+
+do {
+
+} while ()
+
+
+
+
+
+
+
+
+echo "Menggunakan For";
+echo "<br/>";
+for ($level; $level <= $levelMaks; $level++){
+    echo "level hero:".$level;
+    echo " <br />";
+}
+
+// echo "Menggunakan While";
+// echo "<br/>";
+// while ($level < $levelMaks) {
+//     $level = $level + 1;
+//     echo "level Hero: ".$level;
+//     echo "<br/>";
+// }
